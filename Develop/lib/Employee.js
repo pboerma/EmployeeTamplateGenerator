a92b8dedec9c31ca7210f806965db54431bc466b
Pboerma <boerma.pieter@gmail.com>
2020-12-08 14:39:42 -0500

    first commit

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,23 @@
+// CODE: class with constructor (inputs, this.inputs for what we need for an Employee) 
+
+class Employee {
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName(){
+        return this.name;
+    }
+    getId(){
+        return this.id;
+    }
+    getEmail(){
+        return this.email;
+    }
+    getRole(){
+        return "Employee";
+    }
+}
+
+module.exports = Employee;
